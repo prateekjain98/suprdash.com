f4f33fc86f739f37bfcc23a106a6e5f60a1c32d0
prateekjain98 <prateek9jain8@gmail.com>
2025-05-05 14:40:53 +0530

    Error fix

--- src/layouts/shortcodes/Youtube.tsx
+++ src/layouts/shortcodes/Youtube.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
-import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+// import React from "react";
+// import LiteYouTubeEmbed from "react-lite-youtube-embed";
+// import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const Youtube = ({
-  id,
-  title,
-  ...rest
-}: {
-  id: string;
-  title: string;
-  [key: string]: any;
-}) => {
-  return (
-    <LiteYouTubeEmbed
-      wrapperClass="yt-lite rounded-lg"
-      id={id}
-      title={title}
-      {...rest}
-    />
-  );
-};
+// const Youtube = ({
+//   id,
+//   title,
+//   ...rest
+// }: {
+//   id: string;
+//   title: string;
+//   [key: string]: any;
+// }) => {
+//   return (
+//     <LiteYouTubeEmbed
+//       wrapperClass="yt-lite rounded-lg"
+//       id={id}
+//       title={title}
+//       {...rest}
+//     />
+//   );
+// };
 
-export default Youtube;
+// export default Youtube;
